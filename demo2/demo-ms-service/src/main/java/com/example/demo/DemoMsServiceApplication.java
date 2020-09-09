96e0a96ac05f143018bbf6315477a7f1001e3e66
Shinde, Gautam <Gautam.Shinde@naunce.com>
2020-09-09 15:43:09 +0530

    demo micro service

---
+++ demo2/demo-ms-service/src/main/java/com/example/demo/DemoMsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMsServiceApplication.class, args);
+	}
+
+}
